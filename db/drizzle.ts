3fc9f212536e67ab2626b72fd518f5ca44bdc48f
maxingmin <ma_xingmin@163.com>
2024-06-26 13:55:44 +0000

    ✨ feat(db): add mysql schema

---
+++ db/drizzle.ts
@@ -0,0 +1,14 @@
+
+import { mysqlTable } from 'drizzle-orm/mysql-core';
+
+export const dataPool = mysqlTable('data_pool', {
+
+});
+
+export const taskPool = mysqlTable('task_pool', {
+
+});
+
+export const algorithmPool = mysqlTable('algorithm_pool', {
+
+});
